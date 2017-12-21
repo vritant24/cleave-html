b993cad2c00db0cda5eb08b1fac47a01ba5a6f02
vritant24 <vritoku@gmail.com>
2017-12-21 18:20:18 -0500

    added comments to typecheck file

--- helpers/typecheck.js
+++ helpers/typecheck.js
@@ -1,9 +1,12 @@
+// An array of all possible types
 const types = ["undefined", "object", "boolean", "number", "string", "symbol", "function"];
 
+// A function for strings to capitalise the first letter
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
+// Generates typecheck function for the passed in string
 function generateTypeCheck(type) {
     return function (token) {
         if(token && typeof token === type) {
@@ -13,6 +16,7 @@ function generateTypeCheck(type) {
     }
 };
 
+// Export an object of typecheck functions for all types
 module.exports = (() => {
     var obj = {};
     for(var s of types) {
