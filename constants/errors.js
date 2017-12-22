bcc7e60d87b1739d345b13b0001ec93bde8277d4
vritant24 <vritoku@gmail.com>
2017-12-21 22:21:05 -0500

    added tests for check config

--- constants/errors.js
+++ constants/errors.js
@@ -1,11 +1,22 @@
 const ERROR = "Error: ";
 
 const FILE_NOT_FOUND    = ERROR + "File not found";
+const INVALID_ARGUMENT  = ERROR + "Invalid argument to function"
+
 const CONFIG_NOT_FOUND  = ERROR + "Config file not found";
 const INVALID_JSON      = ERROR + "Config file needs to be of valid JSON.";
+const INVALID_SRC       = ERROR + "Invalid source attribute in config. Should be array of nonempty strings";
+const INVALID_DST       = ERROR + "Invalid destination attribute in config. Should be a nonempty string.";
+
+const config = {
+    CONFIG_NOT_FOUND,
+    INVALID_JSON,
+    INVALID_SRC,
+    INVALID_DST,
+}
 
 module.exports = {
     FILE_NOT_FOUND,
-    CONFIG_NOT_FOUND,
-    INVALID_JSON
+    INVALID_ARGUMENT,
+    config,
 }
--- reader/config_reader.js
+++ reader/config_reader.js
@@ -1,17 +1,18 @@
 const fs        = require('fs');
-const type      = require("../helpers/typecheck");
 const e         = require("../constants/errors");
+const _         = require("../constants/globals");
+const type      = require("../helpers/typecheck");
 
 /**
  * Reads the object from the given json file and return is
  * @param {Name of the config file} file_name 
  */
 const jsonReader = (file_name) => {
-    var config; //stores the json object
+    var config = null; //stores the json object
 
     // check validity of the argument
-    if(!file_name && type.isString(file_name)) {
-        return config;
+    if(!file_name || !type.isString(file_name)) {
+        throw(e.INVALID_ARGUMENT);
     }
 
     try {
@@ -19,7 +20,7 @@ const jsonReader = (file_name) => {
         config = fs.readFileSync(file_name, 'utf8');
     } catch(err) {
         // If error in reading file
-        throw(e.CONFIG_NOT_FOUND);
+        throw(e.config.CONFIG_NOT_FOUND);
     }
 
     try {
@@ -27,12 +28,61 @@ const jsonReader = (file_name) => {
         config = JSON.parse(config)
     } catch(err) {
         // If given file does not have a JSON object
-        throw(e.INVALID_JSON);
+        throw(e.config.INVALID_JSON);
     }
 
     return config;
 }
 
+/**
+ * Checks whether given config is properly formatted
+ * @param {Config object read from config file} config 
+ */
+const checkConfig = (config) => {
+    // Check for valid argument
+    if(!config || !type.isObject(config)) {
+        throw e.INVALID_ARGUMENT;
+    }
+
+    // Check if source exists and is an array
+    if(!config.source || !type.isArray(config.source)) {
+        throw(e.config.INVALID_SRC);
+    }
+
+    // Check if the elements of source are strings
+    config.source.forEach( s => {
+        if(!type.isString(s)) {
+            console.log(typeof s);
+            throw(e.config.INVALID_SRC);
+        }
+    });
+
+    // Check if destination exists and is a string
+    if(!config.destination || !type.isString(config.destination)) {
+        throw(e.config.INVALID_DST);
+    }
+
+}
+// console.log(checkConfig({source: "", destination: "dst"}));
+/**
+ * Main method of the config file
+ */
+const getConfig = () => {
+    const file_name = _.CONFIG_FILE;
+
+    // Get the config from the JSON file
+    const config = jsonReader(file_name);
+
+    //check if the config is properly formatted
+    checkConfig(config);
+
+    return config;
+}
+
+
+
 module.exports = {
-    jsonReader
+    jsonReader,
+    checkConfig,
+    getConfig
 };
