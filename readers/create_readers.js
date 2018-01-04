7a621fb34051d5b3145f0440adb573433064ee1b
marsha88 <marsha88@purdue.edu>
2018-01-03 20:21:54 -0500

    finalized first version of getTag and added separate TestProj folder for testing parser

--- readers/create_readers.js
+++ readers/create_readers.js
@@ -1,4 +1,8 @@
+/* Tests if a character is a whitespace character */
 const isSpace = c => /\s/.test(c);
+
+/* Currently this allows html tags to contain any characters that aren't spaces */
+const isValidTagCharacter = c => (c !== '/' && c !== '>' && !isSpace(c))
 
 function Reader(file) {
   this.file = file;
@@ -6,32 +10,43 @@ function Reader(file) {
 }
 
 /* Return next character in file stream and increments the Reader's index */
-Reader.prototype.next = function() {
+Reader.prototype.nextChar = function() {
   return this.file[this.index++];
 };
 
-/* Returns next non-space character */
-Reader.prototype.nextToken = function() {
-  while(isSpace(this.peek())) {
-    this.next();
-  }
-  return this.next();
-};
-
 /* Returns next character in file without advancing Reader's index */
-Reader.prototype.peek = function() {
+Reader.prototype.peekChar = function() {
   return this.file[this.index];
 };
 
-/* Returns the next HTML tag as a string. (Reads next word until space character is found) */
+/* Returns next non-space character */
+Reader.prototype.next = function() {
+  while(isSpace(this.peekChar())) {
+    this.nextChar();
+  }
+  return this.nextChar();
+};
+
+/* Returns next non-space character in file without advancing Reader's index */
+Reader.prototype.peek = function() {
+  let peekIndex = this.index;
+  while(isSpace(this.peekChar())) {
+    peekIndex++;
+  }
+  return this.file[peekIndex];
+};
+
+/* Returns the next HTML tag as a string. (Reads next word until space character or closing tag charcters found) */
 Reader.prototype.getTag = function() {
+  /* Trim whitespace */
+  while(isSpace(this.peekChar())) { this.nextChar(); }
+
   let tag = "";
-  while(!isSpace(this.peek())) {
-    tag += this.next();
+  while(isValidTagCharacter(this.peekChar())) {
+    tag += this.nextChar();
   }
   return tag;
 };
-
 
 const createReaders = files => files.map(file => new Reader(file));
 
